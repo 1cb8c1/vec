407496b4b60e9a288f146fffad53bd1f48afe245
1cb8c1 <6art10miejm0ty1@gmail.com>
2026-01-12 21:13:45 +0000

    fix: missing vec3_q_ prefix in functions

--- include/vector.h
+++ include/vector.h
@@ -7,11 +7,11 @@ struct vec3_q {
     __float128 z;
 };
 
-struct vec3_q add(struct vec3_q v1, struct vec3_q v2);
-struct vec3_q sub(struct vec3_q v1, struct vec3_q v2);
-struct vec3_q scale(struct vec3_q v, __float128 s);
- __float128   dot(struct vec3_q v1, struct vec3_q v2);
-struct vec3_q mul(struct vec3_q v1, struct vec3_q v2);
-struct vec3_q cross(struct vec3_q v1, struct vec3_q v2);
+struct vec3_q vec3_q_add(struct vec3_q v1, struct vec3_q v2);
+struct vec3_q vec3_q_sub(struct vec3_q v1, struct vec3_q v2);
+struct vec3_q vec3_q_scale(struct vec3_q v, __float128 s);
+ __float128   vec3_q_dot(struct vec3_q v1, struct vec3_q v2);
+struct vec3_q vec3_q_mul(struct vec3_q v1, struct vec3_q v2);
+struct vec3_q vec3_q_cross(struct vec3_q v1, struct vec3_q v2);
 
 #endif // VECTOR_H
